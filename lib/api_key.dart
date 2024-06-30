1517459134b5c8b0c179521ff3ccf29d9c773a1f
laguierre <laguierre@gmail.com>
2024-06-30 18:57:22 -0300

    Eliminado el API KEY

--- lib/api_key.dart
+++ lib/api_key.dart
@@ -1,6 +1,6 @@
 //Put you API Key here
 //The Movie DB
-const myApiKey = '0d3ef5f052d5ec394f414b4bac7d1906';
-const dbEmail = 'themoviedb@themoviedb.com';
-const dbPass = 'themoviedb';
+const myApiKey = '';
+const dbEmail = '';
+const dbPass = '';
 
